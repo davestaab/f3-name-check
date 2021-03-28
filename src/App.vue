79ba9cc8b1e88168dad51b16924fbc93d6760938
Dave Staab <davestaab@gmail.com>
2021-03-28 08:37:37 -0400

    setup start of UI

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <h1>F3 Knoxville Name Check</h1>
+    <input>
+    <p v-for="r in results" v-if="results.length !== 0">
+      {{ r.name }} posted {{ r.count }} times
+    </p>
+    <p v-else>
+      No Results
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent, onMounted, ref} from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    const results = ref([]);
+    onMounted(async () => {
+      try {
+        const resp = await fetch('/api/user-search?search=pool')
+        results.value = await resp.json();
+      } catch (e) {
+        console.log(e);
+      }
+    });
+    return {results}
   }
 })
 </script>
