2bab8b20b5ef38f786c4c56438780f72446ac6e7
Dave Staab <davestaab@gmail.com>
2021-04-01 09:13:29 -0400

    added tailwind and styled results

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    <h1>F3 Knoxville Name Check</h1>
-<!--    <input>-->
-    <p v-for="r in results" v-if="results.length !== 0">
-      {{ r.name }} posted {{ r.count }} times
-    </p>
-    <p v-else>
-      No Results
-    </p>
+  <div class="bg-blueGray-100 min-h-screen">
+    <div class="container mx-auto bg-white min-h-screen pb-8">
+      <div class="flex flex-row items-center bg-blueGray-600">
+        <img src="/f3-logo-60x60.png" class="h-12 pr-3" alt="F3 Logo"/>
+        <div class="text-2xl font-bold text-blueGray-50">
+          Knoxville
+        </div>
+      </div>
+      <div class="flex">
+        <div class="text-4xl flex-1 text-center font-sans p-4 text-blueGray-800">
+          Name Check
+        </div>
+      </div>
+      <div class="flex flex-1 justify-center mb-4">
+        <input class="border border-blueGray-200 rounded-tl-lg rounded-bl-lg p-2" v-model="name"
+               @keydown.enter="search">
+        <button class="px-4 py-2 border rounded-br-lg rounded-tr-lg border-l-0 border-blueGray-200" @click="search">
+          Search
+        </button>
+      </div>
+      <!--          <p v-for="r in results" v-if="results.length !== 0">-->
+      <!--            {{ r.name }} posted {{ r.count }} times-->
+      <!--          </p>-->
+
+      <div v-for="r in results" :key="r.id" v-if="results.length"
+           class="py-8 px-8 max-w-sm mx-auto bg-blueGray-600 rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:space-y-0 sm:space-x-6 mb-4">
+        <div class="space-y-2">
+          <div class="space-y-0.5">
+            <p class="text-lg text-white font-semibold">
+              {{ r.name }}
+            </p>
+            <p class="text-blueGray-300 font-medium">
+              posted {{ r.count }} times
+            </p>
+          </div>
+        </div>
+      </div>
+      <div v-else class="text-center">
+        No Results
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,26 +51,27 @@ export default defineComponent({
   components: {},
   setup() {
     const results = ref([]);
+    const name = ref('');
     onMounted(async () => {
-      try {
-        const resp = await fetch('/api/user-search?search=pool')
-        results.value = await resp.json();
-      } catch (e) {
-        console.log(e);
-      }
+      results.value = await checkName('pool');
     });
-    return {results}
+
+    async function search() {
+      results.value = await checkName(name.value);
+    }
+
+    return {results, search, name}
   }
 })
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+async function checkName(name: string) {
+  try {
+    if (name === '') return [];
+    const resp = await fetch(`/api/user-search?search=${name}`)
+    return await resp.json();
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
 }
-</style>
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      blueGray: colors.blueGray,
+      coolGray: colors.coolGray,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
